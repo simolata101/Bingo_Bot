bba16d5619928cf3ae69bfb70ad1e6f225c2f114
Reagan Simolata <94303681+simolata101@users.noreply.github.com>
2025-06-22 14:56:19 +0800

    with linux 64

--- Pokefriends/obj/Debug/net6.0/Pokefriends.AssemblyInfo.cs
+++ Pokefriends/obj/Debug/net6.0/Pokefriends.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokefriends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22f5ea14d19a68798ccc29275684bcb3b9d9209")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62334c9ea3a0b4255cbaea38e75091b3963f9394")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokefriends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokefriends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
